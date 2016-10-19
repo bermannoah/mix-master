afdf396e406878d3d1d33dc8953afcccde5d5781
Noah Berman <noah@noahberman.org>
2016-10-18 19:55:37 -0600

    song edit function built in

--- spec/features/user_edits_a_song_spec.rb
+++ spec/features/user_edits_a_song_spec.rb
@@ -3,32 +3,38 @@ require 'rails_helper'
 RSpec.feature "User edits a song" do
   scenario "they have an edit button" do
     artist = create(:artist)
-    song = create(:song)
+    song = "The Mother We Share"
 
     visit artist_path(artist)
-    click_on "The Mother We Share"
+    click_on "New song"
+    fill_in "song_title", with: song
+    click_on "Create Song"
     expect(page).to have_content "The Mother We Share"
   end
   
   scenario "they can access the edit page" do
     artist = create(:artist)
-    song = create(:song)
+    song = "The Mother We Share"
     
     visit artist_path(artist)
-    click_on "The Mother We Share"
+    click_on "New song"
+    fill_in "song_title", with: song
+    click_on "Create Song"
     click_on "Edit"
     expect(page).to have_content "Title"
   end
   
-  scenario "they can access the edit page" do
+  scenario "they can edit with the edit page" do
     artist = create(:artist)
     song = create(:song)
     new_song_name = "Bury It"
     
     visit artist_path(artist)
-    click_on "The Mother We Share"
+    click_on "New song"
+    fill_in "song_title", with: song
+    click_on "Create Song"
     click_on "Edit"
-    expect(page).to have_content "Title"
+    expect(page).to have_content "Title"    
     fill_in "song_title", with: new_song_name
     click_on "Update Song"
     expect(page).to have_content "Bury It"
